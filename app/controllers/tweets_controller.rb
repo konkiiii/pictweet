82a9f37033136244ec923078a18f0e62dbf2cd87
konkiiii <kintaro0827@icloud.com>
2022-11-14 21:52:08 +0900

    編集ページの実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -12,6 +12,9 @@ class TweetsController < ApplicationController
     tweet = Tweet.find(params[:id])
     tweet.destroy
   end
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
   private
   def tweet_params
     params.require(:tweet).permit(:name, :image, :text)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'tweets#index' #追記
-  resources :tweets, only: [:index, :new, :create, :destroy]
+  resources :tweets, only: [:index, :new, :create, :destroy, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
