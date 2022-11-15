0c59927b9ea2b53cbfef40caaa7f32bfa18876ab
konkiiii <kintaro0827@icloud.com>
2022-11-15 21:59:12 +0900

    メソッドによるコード省略化

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:edit, :show]
+
   def index
     @tweets = Tweet.all
   end
@@ -13,17 +15,18 @@ class TweetsController < ApplicationController
     tweet.destroy
   end
   def edit
-    @tweet = Tweet.find(params[:id])
   end
   def update
     tweet = Tweet.find(params[:id])
     tweet.update(tweet_params)
   end
   def show
-    tweet = Tweet.find(params[:id])
   end
   private
   def tweet_params
     params.require(:tweet).permit(:name, :image, :text)
+  end
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 end
