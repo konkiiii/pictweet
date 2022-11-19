dd16347b9aa9de4b017fd64ba88180864db0214f
konkiiii <kintaro0827@icloud.com>
2022-11-19 18:15:40 +0900

    マイページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: 'tweets#index' #追記
   # resources :tweets, only: [:index, :new, :create, :destroy, :edit, :update, :show]
   resources :tweets
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: :show
 end
