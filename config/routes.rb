f68af9d2ef1ce51c44cc3a4e2af09e41673f0642
konkiiii <kintaro0827@icloud.com>
2022-11-21 21:21:02 +0900

    アソシエーションとcreateのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'tweets#index' #追記
   # resources :tweets, only: [:index, :new, :create, :destroy, :edit, :update, :show]
-  resources :tweets
+  resources :tweets do
+    resources :comments, only: :create
+  end
   resources :users, only: :show
 end
